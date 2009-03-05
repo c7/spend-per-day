a15289c9653a60cd5816ea389f0ba950ff13218f
Peter Hellberg <peter@c7.se>
2009-03-05 01:56:11 +0100

    Added generate_and_open task

--- rakefile.rb
+++ rakefile.rb
@@ -1,5 +1,13 @@
-task :default => :test
+task :default => :generate_and_open
 
 task :test do
   require File.dirname(__FILE__) + '/test/all_tests.rb'  
+end
+
+task :generate_and_open do
+  # Require the script
+  require File.dirname(__FILE__) + '/lib/spend-per-day'
+  
+  # Open the images
+  `open balance.png spend-per-day.png`
 end
